f8f394550197a2de7cf222229eafa637a7c8189d
Bohdan Kovalchuk <bohd@cypress.com>
2019-10-07 19:50:13 +0300

    - filtering targets

--- boot/cypress/MCUBootApp/targets.mk
+++ boot/cypress/MCUBootApp/targets.mk
@@ -29,17 +29,15 @@
 #	- CY8CKIT_062_WIFI_BT
 #	- more to come
 CY8CPROTO-062-4343W := 0x1901
-CY8CKIT_062_WIFI_BT := 0x1900
+# TODO: CY8CKIT_062_WIFI_BT := 0x1900
 
-TARGET ?= CY8CPROTO-062-4343W
+# default TARGET
+TARGET ?= CY8CPROTO-062-4343W-M0
+#
+TARGETS := CY8CPROTO-062-4343W-M0 CY8CKIT-062-WIFI-BT-M0
 
-# Determine TARGET to build for and get its related makefiles from BSP
-ifeq ($(TARGET), $(CY8CPROTO-062-4343W))
-	include ./libs/bsp/CY8CPROTO-062-4343W.mk
-
-else ifeq ($(TARGET), $(CY8CKIT_062_WIFI_BT))
-	include ./libs/bsp/CY8CKIT-062-WIFI_BT.mk
-
-# else ifeq ($(TARGET), $(TARGET_NAME))
-	# include ./libs/bsp/TARGET_NAME.mk
+ifneq ($(filter $(TARGET), $(TARGETS)),)
+include ./libs/bsp/TARGET_$(TARGET)/$(TARGET).mk
+else
+$(error Not supported target: '$(TARGET)')
 endif
