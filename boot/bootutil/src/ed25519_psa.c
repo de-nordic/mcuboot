824050909305d4e94740158219fd7c00b5d2bd6e
Dominik Ermel <dominik.ermel@nordicsemi.no>
2024-06-04 17:25:46 +0000

    Key loading fails

--- boot/bootutil/src/ed25519_psa.c
+++ boot/bootutil/src/ed25519_psa.c
@@ -4,6 +4,7 @@
 
 #include <mcuboot_config/mcuboot_config.h>
 #include <psa/crypto.h>
+#include <psa/crypto_types.h>
 
 #define SHA512_DIGEST_LENGTH 64
 
@@ -17,14 +18,31 @@ int ED25519_verify(const uint8_t *message, size_t message_len,
                           const uint8_t signature[64],
                           const uint8_t public_key[32])
 {
-    psa_status_t status; 
+    /* Set to any error */
+    psa_status_t status = PSA_ERROR_BAD_STATE;
+    psa_key_attributes_t key_attr = PSA_KEY_ATTRIBUTES_INIT;
+    psa_key_id_t kid;
 
-    psa_hash_update(NULL, NULL, 0);
     /* Initialize PSA Crypto */
     status = psa_crypto_init();
     if (status != PSA_SUCCESS) {
         printf("Failed to initialize PSA Crypto\n");
-        return;
+        return 0;
     }
-    return 0;
+
+    status = PSA_ERROR_BAD_STATE;
+
+    psa_set_key_type(&key_attr, PSA_KEY_TYPE_ECC_PUBLIC_KEY(PSA_ECC_FAMILY_TWISTED_EDWARDS));
+    psa_set_key_usage_flags(&key_attr, PSA_KEY_USAGE_VERIFY_MESSAGE); 
+    psa_set_key_algorithm(&key_attr, PSA_ALG_ECDSA(PSA_ALG_SHA_512));
+    status = psa_import_key(&key_attr, public_key, 32, &kid);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to import key with %d\n", status);
+    }
+
+    status = psa_destroy_key(kid);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to destroy key with %d\n", status);
+    }
+    return 1;
 }
